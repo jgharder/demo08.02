4e1ad21c0a39f5ff7106af85ca11143d47ddd0fa
JGHarder <Jacob.gade.harder@gmail.com>
2021-02-08 15:42:54 +0100

    Første push tilføjet ecco og calc metoder og endpoints

--- src/main/java/hello/controller/MyFirstController.java
+++ src/main/java/hello/controller/MyFirstController.java
@@ -1,7 +1,10 @@
 package hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Locale;
 
 @Controller
 
@@ -11,5 +14,23 @@ public class MyFirstController {
     @ResponseBody
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/ecco")
+    @ResponseBody
+    public String ecco(@RequestParam String input){
+        return input.toUpperCase();
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    public String calc(@RequestParam int input){
+        return "" + input*input;
     }
 }
