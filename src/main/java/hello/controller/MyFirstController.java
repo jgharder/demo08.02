01a78c5e7a8a5f3fc1dafd71ca93b5defc27e51c
JGHarder <Jacob.gade.harder@gmail.com>
2021-02-08 15:21:10 +0100

    Initial commit

---
+++ src/main/java/hello/controller/MyFirstController.java
@@ -0,0 +1,15 @@
+package hello.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class MyFirstController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "index";
+    }
+}
